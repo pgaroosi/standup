14ab30bc4bbb0a06c83014ac26cd28cb16c97306
pgaroosi <113352961+pgaroosi@users.noreply.github.com>
2023-12-03 13:27:34 +0330

    Release V1 Standup.py

--- Standup.py
+++ Standup.py
@@ -9,7 +9,15 @@ pygame.init()
 timer = pygame.time.Clock()
 
 # Set reminder interval to 30 minutes
-reminder_interval = 30 * 60  # Seconds
+inttime = int(input("The reminder repeats every(in minutes):"))
+reminder_interval = inttime * 60  # Convert to seconds
+if inttime < 1:
+    print("Error, Wrong time! example: 30")
+    quit()
+elif inttime == 1:
+    print("Ok, Will remind you in",inttime,"minute.")
+else:
+    print("Ok, Will remind you in",inttime,"minutes.")
 
 # Set up the sound
 stand_up_sound = pygame.mixer.Sound("stand_up_sound.mp3")
@@ -26,7 +34,7 @@ def show_popup():
     popup.wm_attributes("-topmost", True)  # Keep the pop-up on top
 
     # Add a label to the pop-up window
-    label = tkinter.Label(popup, text="It's Been 30 Minutes. Stand up already! Don't you think It's essensial to stand up for a bit?")
+    label = tkinter.Label(popup, text="It's Been 30 Minutes. Stand up already!\n\nDon't you think It's essensial to stand up for a bit?")
     label.pack()
 
     # Create a button to close the pop-up window
@@ -50,8 +58,12 @@ while True:
         # Wait for a short period to avoid CPU overload
         time.sleep(0.1)
 
-    # Play the reminder sound and show notification
+    # Play the reminder sound
     stand_up_sound.play()
+    now = time.time()
+    readable_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
+    print("\nReminder ran on ",readable_time)
+    # Play the reminder popup
     show_popup()
 
     # Wait for the sound to finish playing
